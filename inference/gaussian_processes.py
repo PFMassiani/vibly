bd285981be06360818baa0e03777a2ca9c18f4de
Pierre-François Massiani <pfmassiani@gmail.com>
2020-04-29 14:48:20 +0200

    Implements tests for CustomGP. Corrects basic errors in CustomGP. Ensures that arguments are torch.Tensors when calling a CustomGP object.

--- inference/gaussian_processes.py
+++ inference/gaussian_processes.py
@@ -6,15 +6,15 @@ def ensure_tensor(x):
     if isinstance(x,torch.Tensor):
         return x
     elif isinstance(x,ndarray):
-        return torch.from_numpy(x.astype(float))
+        return torch.from_numpy(x.astype(float)).float()
     else:
         return torch.Tensor(x)
 
 class CustomGP(gpytorch.models.ExactGP):
     def __init__(self,train_x,train_y,likelihood,mean_module,covar_module):
-        train_x = ensure_tensor(train_x)
-        train_y = ensure_tensor(train_y)
-        super(CustomGP,self).__init__(train_x,train_y,likelihood)
+        self.train_x = ensure_tensor(train_x)
+        self.train_y = ensure_tensor(train_y)
+        super(CustomGP,self).__init__(self.train_x,self.train_y,likelihood)
         self.mean_module = mean_module
         self.covar_module = covar_module
 
@@ -34,22 +34,17 @@ class CustomGP(gpytorch.models.ExactGP):
         torch.save(self.state_dict(),save_path)
 
     @staticmethod
-    def load(load_path,train_x,train_y,mean_module,covar_module,
-            likelihood = None):
+    def load(load_path,train_x,train_y,likelihood,mean_module,covar_module):
         train_x = ensure_tensor(train_x)
         train_y = ensure_tensor(train_y)
-        if likelihood is None:
-            likelihood = gpytorch.likelihoods.GaussianLikelihood()
 
         state_dict = torch.load(load_path)
         model = CustomGP(train_x,train_y,likelihood,mean_module,covar_module)
         model.load_state_dict(state_dict)
-        return model,likelihood
+        return model
 
-    def optimize_hyperparameters(self,x,y,epochs,optimizer_function=None,
-                                mll_fun=None,**optimizer_kwargs):
-        x = ensure_tensor(x)
-        y = ensure_tensor(y)
+    def optimize_hyperparameters(self,epochs,optimizer_function=None,
+                                mll_function=None,**optimizer_kwargs):
         if optimizer_function is None:
             optimizer_method = torch.optim.Adam
         if mll_function is None:
@@ -58,15 +53,32 @@ class CustomGP(gpytorch.models.ExactGP):
         self.train()
         self.likelihood.train()
 
-        optimizer = optimizer_method((
-                            [{'params':model.parameters()}],
+        optimizer = optimizer_method(
+                            [{'params':self.parameters()}],
                             **optimizer_kwargs
-                        ))
+                        )
         mll = mll_function(self.likelihood,self)
 
         for n in range(epochs):
             optimizer.zero_grad()
-            output = model(x)
-            loss = -mll(output,y)
+            output = self(self.train_x)
+            loss = -mll(output,self.train_y)
             loss.backward()
             optimizer.step()
+
+    def infer(self,x,gp_only=False):
+        x = ensure_tensor(x)
+
+        self.eval()
+        self.likelihood.eval()
+
+        with torch.no_grad(),gpytorch.settings.fast_pred_var():
+            if gp_only:
+                return self(x)
+            else:
+                return self.likelihood(self(x))
+
+    def __call__(self,*args,**kwargs):
+        args = map(ensure_tensor,args)
+        kwargs = {k:ensure_tensor(v) for k,v in kwargs.items()}
+        return super(CustomGP,self).__call__(*args,**kwargs)
---
+++ test/test_gaussian_processes.py
@@ -0,0 +1,120 @@
+import unittest
+import gpytorch
+import numpy as np
+import tempfile
+import os
+import warnings
+
+from inference import gaussian_processes as gps
+
+class TestCustomGP(unittest.TestCase):
+
+    def assertAllClose(self,x,y,tolerance=2e-1):
+        self.assertTrue(x.shape == y.shape)
+        x = x.ravel()
+        y = y.ravel()
+        for k in range(len(x)):
+            self.assertTrue(np.abs(x[k]-y[k])<tolerance)
+    def test_initialization(self):
+        train_x = np.linspace(0,1,9).reshape((3,3))
+        train_y = np.sin(2*np.pi*train_x) + np.random.randn(9).reshape((3,3))*0.2
+
+        mean_module = gpytorch.means.ConstantMean()
+        prior = gpytorch.priors.NormalPrior(2,1)
+        covar_module = gpytorch.kernels.ScaleKernel(
+                            gpytorch.kernels.CosineKernel(
+                                period_length_prior = prior
+                            )
+                        )
+        likelihood = gpytorch.likelihoods.GaussianLikelihood()
+        gp = gps.CustomGP(train_x,train_y,likelihood,mean_module,covar_module)
+
+        gp.covar_module.base_kernel.period_length = prior.mean
+
+        self.assertEqual(gp.likelihood,likelihood)
+        self.assertEqual(gp.mean_module,mean_module)
+        self.assertEqual(gp.covar_module,covar_module)
+        self.assertEqual(gp.covar_module.base_kernel.period_length.item(),2)
+
+    def test_save_load(self):
+        train_x = np.linspace(0,1,9).reshape((3,3))
+        train_y = np.sin(2*np.pi*train_x) + np.random.randn(9).reshape((3,3))*0.2
+        def get_blank_structure():
+            mean_module = gpytorch.means.ConstantMean()
+            covar_module = gpytorch.kernels.ScaleKernel(
+                                gpytorch.kernels.CosineKernel()
+                            )
+            likelihood = gpytorch.likelihoods.GaussianLikelihood()
+
+            return likelihood,mean_module,covar_module
+
+        gp = gps.CustomGP(train_x,train_y,*get_blank_structure())
+
+        # We change a parameter to check that it is saved
+        gp.covar_module.base_kernel.period_length = 100
+        gp.likelihood.noise_covar.noise = 10
+
+        save_file = tempfile.NamedTemporaryFile(suffix='.pth').name
+        gp.save(save_file)
+
+        self.assertTrue(os.path.isfile(save_file))
+
+        loaded_gp = gps.CustomGP.load(save_file,train_x,train_y,*get_blank_structure())
+        self.assertEqual(
+                loaded_gp.covar_module.base_kernel.period_length,
+                gp.covar_module.base_kernel.period_length
+            )
+        self.assertEqual(
+            loaded_gp.likelihood.noise_covar.noise,
+            gp.likelihood.noise_covar.noise
+        )
+
+    def test_optimization_1d(self):
+        warnings.simplefilter('ignore',gpytorch.utils.warnings.GPInputWarning)
+
+        x = np.linspace(0,1,101)
+        y = x**2
+
+        # prior = gpytorch.priors.NormalPrior(0.8, 1)
+        mean_module = gpytorch.means.ConstantMean()
+        covar_module = gpytorch.kernels.ScaleKernel(
+                            gpytorch.kernels.PolynomialKernel(power=2)
+                        )
+        likelihood = gpytorch.likelihoods.GaussianLikelihood()
+
+        gp = gps.CustomGP(x,y,likelihood,mean_module,covar_module)
+        # gp.mean_module.mean = prior.mean
+
+        gp.optimize_hyperparameters(epochs=20)
+
+        predictions = gp.infer(x)
+
+        self.assertAllClose(y,predictions.mean.numpy())
+
+    def test_optimization_2d(self):
+        warnings.simplefilter('ignore',gpytorch.utils.warnings.GPInputWarning)
+
+        x1 = np.linspace(0,1,11)
+        x2 = np.linspace(0,1,11)
+        x = np.dstack(np.meshgrid(x1,x2))
+        x_l = x.reshape((-1,2))
+        y = (x_l**2).sum(axis=1)#np.sin(x_l[:,0] * 2*np.pi - x_l[:,1] *2*np.pi)
+
+        mean_module = gpytorch.means.ConstantMean()
+        covar_module = gpytorch.kernels.ScaleKernel(
+                            gpytorch.kernels.PolynomialKernel(power=2)
+                        )
+        likelihood = gpytorch.likelihoods.GaussianLikelihood()
+
+        gp = gps.CustomGP(x_l,y,likelihood,mean_module,covar_module)
+
+        gp.optimize_hyperparameters(epochs=20)
+
+        predictions = gp.infer(x_l)
+
+        means = predictions.mean
+
+        self.assertAllClose(y,predictions.mean.numpy())
+
+if __name__ == '__main__':
+    unittest.main()
